c827424469e9e5e134d21847a511a72bc619b8f0
Thestaloz <oskr-75@hotmail.com>
2025-03-04 16:18:09 -0600

    primer commit

---
+++ Carpeta_1/Console-Log.js
@@ -0,0 +1,16 @@
+// Ejercicios practicos de console.log
+// Console.log practical exercises
+
+// (1)
+console.log("Hola mundo")
+
+// (2)
+
+console.log("   a   ")
+console.log("  b c  ")
+console.log(" d e f ")
+console.log("g h i j")
+console.log("   k   ")
+
+
+//
