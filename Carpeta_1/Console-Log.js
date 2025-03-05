cf3180c25577c709de2a8bcef96aa89801625bfd
Thestaloz <oskr-75@hotmail.com>
2025-03-05 10:29:13 -0600

    Ejercicios de console.log

--- Carpeta_1/Console-Log.js
+++ Carpeta_1/Console-Log.js
@@ -13,4 +13,41 @@ console.log("g h i j")
 console.log("   k   ")
 
 
-//
+// (3)
+
+console.log("Ingredientes")
+console.log("1 taza de arina de trigo")
+console.log("2 cucharadas de azucar")
+console.log("1 cucharada de polvo para hornear")
+console.log("1/2 cucharada de sal")
+console.log("3/4 de leche")
+console.log("1 huevo")
+console.log("2 cucharadas de mantequilla deretida")
+
+console.log("Instrucciones")
+console.log("1. Precalentar el sarten")
+console.log("2. Mezclar en un tazon la harian, azucal, polvo para hornear y la sal")
+console.log("3. En otro tazon batir la leche, el huevo, la mantequilla derretida y el estracto de vainilla")
+console.log("4. Mezclar todos los ingredientes")
+console.log("5. Engrase el sarten, vierta 1/4 parte de la mezcla en el. Cocina hasta que aparezcan burbujas en la superficie y los bordes empiecen a verse cocidos, luego da la vuelta y cocina hasta que el otro lado esté dorado")
+console.log("6. Sirve los hot cakes calientes con tus ingredientes favoritos como miel de maple, frutas frescas, crema batida, nueces o chocolate.")
+
+// (4) 
+
+console.log("==============================")
+console.log(" ")
+console.log("       McDONALD's RECIBO")
+console.log(" ")
+console.log("------------------------------")
+console.log(" ")
+console.log("Alimento   Cantidad    Precio ")
+console.log(" ")
+console.log("Hamburguesa    1         35   ")
+console.log("Papas fritas   1         15   ")
+console.log("Refresco       1         10   ")
+console.log(" ")
+console.log("------------------------------")
+console.log("Total                    60$  ")
+console.log(" ")
+console.log("==============================")
+
